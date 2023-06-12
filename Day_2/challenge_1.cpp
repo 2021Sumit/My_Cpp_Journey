7faf845ec1d32627d556a221fa186fdbef2a476c
Sumit Ghosh <77159518+2021Sumit@users.noreply.github.com>
2023-06-13 00:13:42 +0530

    Add files via upload

---
+++ Day_2/challenge_1.cpp
@@ -0,0 +1,17 @@
+// Write a program to assign value and print a variable 
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // create the salary variable and assign 65890 to it
+    int salary = 65890;
+
+    // print the string "salary: "
+    cout << "salary: ";
+
+    // print the salary variable
+    cout << salary << endl;
+
+    return 0;
+}
